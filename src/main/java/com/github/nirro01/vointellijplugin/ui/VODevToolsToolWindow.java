313c9fe992813845d9319b36eb365b9d644fe234
nir.rozenberg <nir.rozenberg@viaccess-orca.com>
2020-11-10 22:03:40 +0200

    tool window works

---
+++ src/main/java/com/github/nirro01/vointellijplugin/ui/VODevToolsToolWindow.java
@@ -0,0 +1,16 @@
+package com.github.nirro01.vointellijplugin.ui;
+
+import javax.swing.*;
+
+public class VODevToolsToolWindow {
+    private JPanel voDevToolsContent;
+    private JButton stopAdminButton;
+
+    public VODevToolsToolWindow() {
+        //stopAdminButton.addActionListener(e -> );
+    }
+
+    public JPanel getVoDevToolsContent() {
+        return voDevToolsContent;
+    }
+}
